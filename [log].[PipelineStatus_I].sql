0df55d976eedda21fc790e00df567ffa8d18705f
Heshan Nanayakkara <kalaruheshan@gmail.com>
2023-12-10 22:26:21 +0530

    Update [log].[PipelineStatus_I].sql

--- [log].[PipelineStatus_I].sql
+++ [log].[PipelineStatus_I].sql
@@ -1,10 +1,4 @@
-/****** Object:  StoredProcedure [log].[PipelineStatus_I]    Script Date: 11/12/2023 1:25:25 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
+-- This SP is used to insert a new record into the [log].[PipelineStatus] table at the start of each pipeline run
 CREATE PROCEDURE [log].[PipelineStatus_I] @DataFactory varchar(100), @Pipeline varchar(100)
 AS
 	INSERT INTO [log].[PipelineStatus] ([DataFactory], [Pipeline], [StartTime])
