cc6974ffec23c2601981f1edf580a2b74c9b2a39
Heshan Nanayakkara <kalaruheshan@gmail.com>
2023-12-10 22:54:34 +0530

    Update [log].[LakeLogs_S].sql

--- [log].[LakeLogs_S].sql
+++ [log].[LakeLogs_S].sql
@@ -1,4 +1,4 @@
--- This SP is used to retrieve values from the log database based on the key value passed as a parameter
+-- This SP is used to retrieve values from the [log].[LakeLogs] table based on the key value passed as a parameter
 CREATE PROCEDURE [log].[LakeLogs_S] @Key VARCHAR(50)
 AS
 	SELECT [Value] FROM [log].[LakeLogs] WHERE [Key] = @Key;
