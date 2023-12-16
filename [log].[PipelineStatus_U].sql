ee7a5a514329368db114ea31dc7bba5b5e4a1887
Heshan Nanayakkara <kalaruheshan@gmail.com>
2023-12-16 13:03:41 +0530

    Update [log].[PipelineStatus_U].sql

--- [log].[PipelineStatus_U].sql
+++ [log].[PipelineStatus_U].sql
@@ -1,10 +1,4 @@
-/****** Object:  StoredProcedure [log].[PipelineStatus_U]    Script Date: 11/12/2023 1:43:21 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
+-- This SP is used to update the record inserted into the [log].[PipelineStatus] table at the start of each pipeline run, based on the pipeline run status
 CREATE PROCEDURE [log].[PipelineStatus_U] @DataFactory varchar(100), @Pipeline varchar(100), @Status varchar(100), @Error varchar(100)
 AS
 	DECLARE @Id AS INT;
